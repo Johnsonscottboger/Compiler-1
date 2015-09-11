efbf3bcc6dda359514718b547e9c6dd6f07b15ff
liaonaizhuo <1615117331@qq.com>
2015-09-11 15:24:42 +0800

    Revert "C#之sample语言编译器"
    This reverts commit 460d24f4b3e9a10daccf093c770ca852055464c9.

--- compiler/Properties/AssemblyInfo.cs
+++ compiler/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Compiler")]
+[assembly: AssemblyTitle("compiler")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Compiler")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("compiler")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("075d9877-7e97-4907-93f7-79a22fec779c")]
+[assembly: Guid("f003e644-68b2-41c9-bad8-4aa1a6223f50")]
 
 // 程序集的版本信息由下面四个值组成:
 //
